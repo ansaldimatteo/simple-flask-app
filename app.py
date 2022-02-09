a0e128a3c34a55739d8318fa406715da4ead1399
Matteo <matteo@ubuntu-pc.station>
2022-02-09 20:48:03 +0100

    Added db references

--- app.py
+++ app.py
@@ -1,29 +1,58 @@
 from flask import Flask
+from flask import request
+import mysql.connector
 from datetime import datetime
 import re
 
 app = Flask(__name__)
 
-@app.route("/")
+def getMysqlConnection():
+    return mysql.connector.connect(
+        host="10.0.1.5",
+        port='3306',
+        user="root",
+        password="SomeRootPassword1!",
+        database="dev"
+    )
+
+@app.route('/', methods=['GET'])
 def home():
-    return "Hello, Flask!"
+    mydb = getMysqlConnection()
+    try:
+        sql = "SELECT * from key_value_table"
+        cur = mydb.cursor()
+        cur.execute(sql)
+        output_json = cur.fetchall()
+    except Exception as e:
+        print("Error in SQL:\n", e)
+    finally:
+        mydb.close()
+    return jsonify(results=output_json)
 
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+@app.route('/<key>', methods=['PUT'])
+def addKey(key):
+    content = request.json
+    value = content['value']
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
+    mydb = getMysqlConnection()
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO key_value_table (keyValue, value) VALUES (%s, %s)"
+    val = (key, value)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    mydb.close()
 
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
+    print(mycursor.rowcount, "record inserted.")
+    return "Add key value pair"
 
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route('/<key>', methods=['GET'])
+def getKey(key):
+    print(key, flush=True)
+    return "Return key value pair " + key
+
+@app.route('/<key>', methods=['DELETE'])
+def deleteKey(key):
+    return "Delete all key value pairs"
 
 if __name__ == "__main__":
     app.run(debug=True)
